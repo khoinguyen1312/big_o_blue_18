08166ac29c28bf5774d0b7a8dde4b2db9c79b63d
Nguyen Phan <khoinguyen1312@gmail.com>
2019-07-06 14:55:26 +0700

    add note

---
+++ 07_Heap/eclipse/src/heap/Promotion2.java
@@ -0,0 +1,87 @@
+package heap;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.PriorityQueue;
+import java.util.Scanner;
+
+public class Promotion2 {
+	public static void main(String[] args) {
+    	Scanner argumentScanner = new Scanner(System.in);
+	
+		int numberOfDays = argumentScanner.nextInt();
+		
+		List<List<Integer>> days = new ArrayList<>();
+		
+		for (int i = 0; i < numberOfDays; i++) {
+			int numberOfThisDay = argumentScanner.nextInt();
+
+			List<Integer> day = new ArrayList<>();
+			
+			for (int j = 0; j < numberOfThisDay; j++) {
+				day.add(argumentScanner.nextInt());
+			}
+			
+			days.add(day);
+		}
+		
+		argumentScanner.close();
+		
+		int cost = calculateCostOfPromotion(days);
+		
+		System.out.println(cost);
+	}
+	
+	public static void test() {
+		calculateCostOfPromotion(Arrays.asList(
+				Arrays.asList(1, 2, 3),
+				Arrays.asList(1, 1),
+				Arrays.asList(10, 5, 5, 1),
+				Collections.emptyList(),
+				Arrays.asList(2)
+			));
+		
+	}
+	
+	public static int calculateCostOfPromotion(List<List<Integer>> days) {
+		/*
+		 * Should use an id for storing and calcualte if this ticket has been checked         
+		 * use an array to store if this ticket has been checked or not -> reduce complexity  
+		 * Ex: Array[Ticket]
+		 * Ticket {
+		 * 	 value,
+		 * 	 id,
+		 *   isChecked
+		 * }
+		 */		
+		
+		PriorityQueue<Integer> maxQueue = new PriorityQueue<>((a, b) -> b.compareTo(a));
+		PriorityQueue<Integer> minQueue = new PriorityQueue<>();
+		
+		int cost = 0;
+		
+		for (List<Integer> day : days) {
+			for (Integer prize : day) {
+				maxQueue.add(prize);
+				minQueue.add(prize);
+			}
+			
+			Integer max = maxQueue.poll();
+			Integer min = minQueue.poll();
+			
+			maxQueue.remove(min);
+			minQueue.remove(max);
+			
+			cost += max - min;
+
+			
+//			System.out.println("Max, Min: " + max + ", " + min + "| max - min = " +  (max - min));
+		}
+		
+//		System.out.println(cost);
+		
+		return cost;
+	}
+}
--- 07_Heap/note.md
+++ 07_Heap/note.md
@@ -10,3 +10,24 @@ minHeapify -> O(log)
 ## Promotion
 minAlreadyIndex[] = //store min index that already seen
 maxAlreadyIndex[] = //store max index that already seen
+
+numberIndexCounter[] = //use to store is number ready to push
+
+## Restaurant Rating
+MinHeap top
+MaxHeap remain
+
+int counter = 0;
+
+for review in reviews
+    if (input == 2)
+        -> print top
+    if (input == 1)
+        remain.add(review)
+        counter++;
+        if (counter % 3 == 0) {
+            top.add(remain.pop)
+        } else {
+            if (top.peek < remain.peek)
+                swap(top.peek, remain.peek)
+        }
