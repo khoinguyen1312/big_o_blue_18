18af63d9b3362b3c31099eaed8cb422f1cb0c2db
Nguyen Phan <khoinguyen1312@gmail.com>
2019-07-13 16:06:15 +0700

    Bellman Ford with test cases

--- 10_bellman_ford/eclipse/src/bellman/BellmanFord.java
+++ 10_bellman_ford/eclipse/src/bellman/BellmanFord.java
@@ -1,19 +1,32 @@
 package bellman;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class BellmanFord {
-	class Edge {
+	static class Edge {
 		public int source;
 		public int target;
 		public int weight;
+		
+		private Edge(int source, int target, int weight) {
+			this.source = source;
+			this.target = target;
+			this.weight = weight;
+		}
+		
+		public static Edge createInstance(int source, int target, int weight) {
+			return new Edge(source, target, weight);
+		}
 	}
 	
 	private final int INF = Integer.MAX_VALUE;
-	private Edge[] graph;
+	private List<Edge> graph;
 	private int numberOfNode;
 	private int[] path;
 	private int[] dist;
 	
-	public BellmanFord(Edge[] graph, int numberOfNode, int numberOfEdge) {
+	public BellmanFord(List<Edge> graph, int numberOfNode) {
 		this.graph = graph;
 		this.numberOfNode = numberOfNode;
 		this.path = new int[numberOfNode];
@@ -22,10 +35,10 @@ public class BellmanFord {
 	
 	public boolean bellmanFord() {
 		for (int i = 0; i < numberOfNode - 1; i++) {
-			for (int j = 0; j < graph.length; j++) {
-				int from = graph[j].source;
-				int to = graph[j].target;
-				int weight = graph[j].weight;
+			for (int j = 0; j < graph.size(); j++) {
+				int from = graph.get(j).source;
+				int to = graph.get(j).target;
+				int weight = graph.get(j).weight;
 				
 				if (dist[from] != INF && dist[from] + weight < dist[to]) {
 					dist[to] = dist[from] + weight;
@@ -34,10 +47,10 @@ public class BellmanFord {
 			}
 		}
 
-		for (int j = 0; j < graph.length; j++) {
-			int from = graph[j].source;
-			int to = graph[j].target;
-			int weight = graph[j].weight;
+		for (int j = 0; j < graph.size(); j++) {
+			int from = graph.get(j).source;
+			int to = graph.get(j).target;
+			int weight = graph.get(j).weight;
 			
 			if (dist[from] != INF && dist[from] + weight < dist[to]) {
 				return false;
@@ -48,6 +61,25 @@ public class BellmanFord {
 	}
 	
 	public static void main(String[] args) {
+		BellmanFord solution = new BellmanFord(
+				Arrays.asList(
+					new Edge(0, 1, 10),
+					new Edge(1, 2, 20),
+					new Edge(2, 3, 30),
+					new Edge(3, 0, -60)
+					),
+				4);
+		System.out.println(solution.bellmanFord());
 		
+
+		solution = new BellmanFord(
+				Arrays.asList(
+					new Edge(0, 1, 1000),
+					new Edge(1, 2, 15),
+					new Edge(2, 1, -42)
+					),
+				3);
+
+		System.out.println(solution.bellmanFord());
 	}
 }
---
+++ 10_bellman_ford/eclipse/src/bellman/Wormholes.java
@@ -0,0 +1,7 @@
+package bellman;
+
+public class Wormholes {
+	public static void main(String[] args) {
+		
+	}
+}
