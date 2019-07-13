f20deda387285b909122fad9e1edff4cf7f8343d
Nguyen Phan <khoinguyen1312@gmail.com>
2019-07-13 16:16:47 +0700

    Use Bellman to solve worm Holes

--- 10_bellman_ford/eclipse/src/bellman/Wormholes.java
+++ 10_bellman_ford/eclipse/src/bellman/Wormholes.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-import bellman.BellmanFord.Edge;
+import bellman.BellmanFordWormholes.Edge;
 
 public class Wormholes {
 	public static void main(String[] args) {
@@ -25,9 +25,9 @@ public class Wormholes {
     			edges.add(Edge.createInstance(source, target, weight));
     		}
     		
-    		BellmanFord solution = new BellmanFord(edges, numberOfNode);
+    		BellmanFordWormholes solution = new BellmanFordWormholes(edges, numberOfNode);
     		
-    		if (solution.bellmanFord()) {
+    		if (solution.bellmanFordWormholes()) {
     			System.out.println("not possible");
     		} else {
     			System.out.println("possible");
@@ -35,5 +35,63 @@ public class Wormholes {
     	}
     	
     	argumentScanner.close();
+	}
+}
+
+class BellmanFordWormholes {
+	static class Edge {
+		public int source;
+		public int target;
+		public int weight;
+		
+		private Edge(int source, int target, int weight) {
+			this.source = source;
+			this.target = target;
+			this.weight = weight;
+		}
+		
+		public static Edge createInstance(int source, int target, int weight) {
+			return new Edge(source, target, weight);
+		}
+	}
+	
+	private final int INF = Integer.MAX_VALUE;
+	private List<Edge> graph;
+	private int numberOfNode;
+	private int[] path;
+	private int[] dist;
+	
+	public BellmanFordWormholes(List<Edge> graph, int numberOfNode) {
+		this.graph = graph;
+		this.numberOfNode = numberOfNode;
+		this.path = new int[numberOfNode];
+		this.dist = new int[numberOfNode];
+	}
+	
+	public boolean bellmanFordWormholes() {
+		for (int i = 0; i < numberOfNode - 1; i++) {
+			for (int j = 0; j < graph.size(); j++) {
+				int from = graph.get(j).source;
+				int to = graph.get(j).target;
+				int weight = graph.get(j).weight;
+				
+				if (dist[from] != INF && dist[from] + weight < dist[to]) {
+					dist[to] = dist[from] + weight;
+					path[to] = from;
+				}
+			}
+		}
+
+		for (int j = 0; j < graph.size(); j++) {
+			int from = graph.get(j).source;
+			int to = graph.get(j).target;
+			int weight = graph.get(j).weight;
+			
+			if (dist[from] != INF && dist[from] + weight < dist[to]) {
+				return false;
+			}
+		}
+		
+		return true;
 	}
 }
