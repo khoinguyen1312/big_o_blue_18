0710753195a9677b949d9dcf312ee179a9791ba6
Nguyen Phan <nguyen.phan1@mservice.com.vn>
2019-08-03 10:36:16 +0700

    Solve Connect The campus

---
+++ 15_Minimum_Spanning_Tree/eclipse/src/minimumspanningtree/CobbledStreets.java
@@ -0,0 +1,18 @@
+package minimumspanningtree;
+
+import java.util.Scanner;
+
+public class CobbledStreets {
+	public static void main(String[] args) {
+		Scanner argumentScanner = new Scanner(System.in);
+		
+		int numberOfProblem = argumentScanner.nextInt();
+		
+		for (int problem = 0; problem < numberOfProblem; problem++) {
+			int priceToPaveOneFurlongStreet = argumentScanner.nextInt();
+			int numberOfBuilding = argumentScanner.nextInt();
+			int numberOfStreet = argumentScanner.nextInt();
+			
+		}
+	}
+}
--- 15_Minimum_Spanning_Tree/eclipse/src/minimumspanningtree/ConnectTheCampus.java
+++ 15_Minimum_Spanning_Tree/eclipse/src/minimumspanningtree/ConnectTheCampus.java
@@ -11,50 +11,52 @@ public class ConnectTheCampus {
 	public static void main(String[] args) {
 		Scanner argumentScanner = new Scanner(System.in);
 		
-		int numberOfBuilding = argumentScanner.nextInt();
-		int x[] = new int[numberOfBuilding];
-		int y[] = new int[numberOfBuilding];
-		
-		for (int i = 0; i < numberOfBuilding; i++) {
-			x[i] = argumentScanner.nextInt();
-			y[i] = argumentScanner.nextInt();
-		}
-		
-		List<List<CampusNode>> graph = new ArrayList<>();
-		
-		for (int i = 0; i < numberOfBuilding; i++) {
-			graph.add(new ArrayList<>());
-		}
-		
-		for (int i = 0; i < numberOfBuilding; i++) {
-			for (int j = 0; j < numberOfBuilding; j++) {
-				if (i != j) {
-					int leftNodeId = i;
-					int rightNodeId = j;
-					double lengthBetweenNodes = Math.sqrt((x[i] - x[j])*(x[i] - x[j]) + (y[i] - y[j])*(y[i] - y[j]));
-					
-					graph.get(leftNodeId).add(new CampusNode(rightNodeId, lengthBetweenNodes));
-					graph.get(rightNodeId).add(new CampusNode(leftNodeId, lengthBetweenNodes));
+		while(argumentScanner.hasNext()) {
+			int numberOfBuilding = argumentScanner.nextInt();
+			int x[] = new int[numberOfBuilding];
+			int y[] = new int[numberOfBuilding];
+			
+			for (int i = 0; i < numberOfBuilding; i++) {
+				x[i] = argumentScanner.nextInt();
+				y[i] = argumentScanner.nextInt();
+			}
+			
+			List<List<CampusNode>> graph = new ArrayList<>();
+			
+			for (int i = 0; i < numberOfBuilding; i++) {
+				graph.add(new ArrayList<>());
+			}
+			
+			for (int i = 0; i < numberOfBuilding; i++) {
+				for (int j = 0; j < numberOfBuilding; j++) {
+					if (i != j) {
+						int leftNodeId = i;
+						int rightNodeId = j;
+						double lengthBetweenNodes = Math.sqrt((x[i] - x[j])*(x[i] - x[j]) + (y[i] - y[j])*(y[i] - y[j]));
+						
+						graph.get(leftNodeId).add(new CampusNode(rightNodeId, lengthBetweenNodes));
+						graph.get(rightNodeId).add(new CampusNode(leftNodeId, lengthBetweenNodes));
+					}
 				}
 			}
-		}
-		
-		
-		int numberOfExistingCable = argumentScanner.nextInt();
-		
-		for (int i = 0; i < numberOfExistingCable; i++) {
-			int leftId = argumentScanner.nextInt() - 1;
-			int rightId = argumentScanner.nextInt() - 1;
 			
-			graph.get(leftId).stream().filter(n -> n.id == rightId).findFirst().get().dist = 0.0;
-			graph.get(rightId).stream().filter(n -> n.id == leftId).findFirst().get().dist = 0.0;
+			
+			int numberOfExistingCable = argumentScanner.nextInt();
+			
+			for (int i = 0; i < numberOfExistingCable; i++) {
+				int leftId = argumentScanner.nextInt() - 1;
+				int rightId = argumentScanner.nextInt() - 1;
+				
+				graph.get(leftId).stream().filter(n -> n.id == rightId).findFirst().get().dist = 0.0;
+				graph.get(rightId).stream().filter(n -> n.id == leftId).findFirst().get().dist = 0.0;
+			}
+			
+			CampusMST solution = new CampusMST(graph);
+			solution.prim(0);
+			System.out.println(String.format("%.2f", DoubleStream.of(solution.distance).sum()));
 		}
 		
 		argumentScanner.close();
-		
-		CampusMST solution = new CampusMST(graph);
-		solution.prim(0);
-		System.out.println(String.format("%.2f", DoubleStream.of(solution.distance).sum()));
 	}
 }
 
